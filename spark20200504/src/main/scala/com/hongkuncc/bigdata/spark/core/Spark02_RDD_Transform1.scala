31e7ff6c6b1ca6d720e6112a1cc3303207767243
hongkuncc <hongkuncc@163.com>
2020-05-05 23:12:08 +0800

    initial

--- spark20200504/src/main/scala/com/hongkuncc/bigdata/spark/core/Spark02_RDD_Transform1.scala
+++ spark20200504/src/main/scala/com/hongkuncc/bigdata/spark/core/Spark02_RDD_Transform1.scala
@@ -1,6 +1,6 @@
 package com.hongkuncc.bigdata.spark.core
 
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
 
 object Spark02_RDD_Transform1 {
@@ -8,16 +8,19 @@ object Spark02_RDD_Transform1 {
      // 1. 创建 SparkConf对象
     val sparkConf = new SparkConf().setAppName("Spark02_RDD_Transform1").setMaster("local[*]")
     // 2. 创建Spark环境连接对象
-    val sc = new SparkConf(sparkConf)
+    val sc = new SparkContext(sparkConf)
 
-    //构建RDD
-    val nullRDD:RDD[Int] = sc.makeRDD(List(1,2,3,4))
+    // 3. 构建RDD
+    val numRDD:RDD[Int] = sc.makeRDD(List(1,2,3,4),numSlices = 2)
 
     // 将RDD通过算子进行转换，变成新的RDD
-    //
+    //转换算子 - map
+    val mapRDD:RDD[Int] = numRDD.map(num =>{num*2})
+    //mapRDD.collect().foreach(println)
+    mapRDD.saveAsTextFile("output")
 
-
-
+    //释放连接
+    sc.stop()
   }
 
 }
