ea605341cae8ff372adea8e0ddd539e35cc0b74e
hongkuncc <hongkuncc@163.com>
2020-05-17 23:07:04 +0800

    initial

--- spark20200504/src/main/scala/com/hongkuncc/bigdata/spark/sql/SparkSQL01_basic.scala
+++ spark20200504/src/main/scala/com/hongkuncc/bigdata/spark/sql/SparkSQL01_basic.scala
@@ -1,16 +1,19 @@
 package com.hongkuncc.bigdata.spark.sql
-import
+import org.apache.spark
+import org.apache.spark.{SparkConf, SparkContext}
 object SparkSQL01_basic {
   def main(args: Array[String]): Unit = {
 
 
     // 创建一个新的 SparkSession 对象
-    val spark: SparkSession = new SparkSession.builder()
-      .master("local[*]")
-      .appName("Word Count")
-      .getOrCreate()
+    val sparkconf = new SparkConf().setAppName("hello world").setMaster("local[*]")
+    val sc = new SparkContext(sparkconf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
     // 导入用到隐式转换.  如果想要使用: $"age" 则必须导入
-    val df = spark.read.json("file://" + ClassLoader.getSystemResource("user.json").getPath)
+    import sqlContext.implicits._
+
+    val df = sqlContext.read.json("file://" + ClassLoader.getSystemResource("user.json").getPath)
     // 打印信息
     df.show
     // 查找年龄大于19岁的
@@ -18,10 +21,10 @@ object SparkSQL01_basic {
 
     // 创建临时表
     df.createTempView("user")
-    spark.sql("select * from user where age > 19").show
+    sqlContext.sql("select * from user where age > 19").show
 
     //关闭连接
-    spark.stop()
+    sc.stop()
 
   }
 
