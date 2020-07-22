3b7daf7f897ed8e255946ba1f868553d6142007c
hongkuncc <hongkuncc@163.com>
2020-07-22 21:59:40 +0800

    initial

---
+++ sparkmall/src/main/scala/com/hongkuncc/offline/bean/CategorySession.scala
@@ -0,0 +1,7 @@
+package com.hongkuncc.offline.bean
+
+case class CategorySession(taskId: String,
+                           categoryId: String,
+                           sessionId: String,
+                           clickCount: Long)
+
