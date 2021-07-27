882203f56ae38a1a01b4e547a98e9f4237967f28
hongkuncc <hongkuncc@163.com>
2021-07-27 21:54:24 +0800

    javautil

--- tqm/src/main/Java/util/DateUtil.java
+++ tqm/src/main/Java/util/DateUtil.java
@@ -5,6 +5,7 @@ import org.joda.time.format.DateTimeFormatter;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -19,6 +20,30 @@ public class DateUtil {
 /*    public static DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern({"yyyyMMddHHmmss");
     public static DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern({"yyyy-MM-dd");
     public static DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern({"yyyyMMdd hh:mm:ss");*/
+
+
+    public static String  getAddDate(int AddDay) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date nowDate = new Date();
+        /*
+         * @Author hongkuncc
+         * @Description 将日期往后加一天
+         * @Date  2021/7/27 21:41
+         * @Param
+         * @Param
+         * @Return
+         * @MethodName
+         */
+
+        Calendar cd = Calendar.getInstance();
+        cd.setTime(nowDate);
+        cd.add(Calendar.DAY_OF_MONTH,AddDay);
+        nowDate = cd.getTime();
+        String addDate = sdf.format(nowDate);
+        return addDate;
+
+
+    }
 
     public static void main(String[] args) {
         String str = "20201213";
